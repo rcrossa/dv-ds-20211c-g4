f4d3895dd1d478780cf4a19a9a791d8aacf4f6bd
rcrossa <rcrossa@hotmail.com>
2021-06-11 21:25:32 -0300

    Primer commit

---
+++ src/main/java/ar/edu/davinci/dvds2021cg4/domain/TipoPrenda.java
@@ -0,0 +1,28 @@
+package ar.edu.davinci.dvds2021cg4.domain;
+
+import lombok.Data;
+
+@Data
+
+public enum TipoPrenda {
+	SACO("Saco"),
+	PANTALON("Pantalon"),
+	CAMISA("Camisa"),
+	CAMPERA("Campera"),
+	TAPADO("Tapado"),
+	CHAQUETA("Chaqueta"),
+	MEDIA("Media"),
+	BUFANDA("Bufanda");
+	
+	private String descripcion;
+
+    private TipoPrenda(String descripcion) {
+    	this.descripcion = descripcion;
+    }
+    
+    public String getDescripcion() {
+    	return descripcion;
+    }
+	
+	
+}
---
+++ src/test/java/ar/edu/davinci/dvds2021cg4/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds2021cg4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
